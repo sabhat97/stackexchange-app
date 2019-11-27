a2bb776b10fab6819c958045477bc143c74d6bae
Sandesh Bhat <sandeshbhat@Sandeshs-MBP.fios-router.home>
2019-11-27 11:13:35 -0500

    added bar and pie chart with dummy data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PieChart from './components/PieChart';
+import BarChart from './components/BarChart';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hello World!</p>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      chartData:{}
+    }
+  }
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData(){
+    // Ajax calls here
+    this.setState({
+      chartData:{
+        labels: ['Java', 'JavaScript', 'C++', 'C', 'MySQL', 'HTML'],
+        datasets:[
+          {
+            label:'Users',
+            data:[
+              617594,
+              181045,
+              153060,
+              106519,
+              105162,
+              95072
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)'
+            ]
+          }
+        ]
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>StackExchange</h2>
+        </div>
+        <table className="table table-bordered">
+          <tbody>
+            <tr>
+              <td className='bar-col'> <BarChart chartData={this.state.chartData} location="" legendPosition="bottom"/></td>
+              <td className='pie-col'><PieChart chartData={this.state.chartData} location="" legendPosition="bottom"/></td>
+            </tr>
+          </tbody>
+        </table>
+       
+        
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/BarChart.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import {Bar} from 'react-chartjs-2';
+
+class BarChart extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      chartData:props.chartData
+    }
+  }
+
+  static defaultProps = {
+    displayTitle:true,
+    displayLegend: true,
+    legendPosition:'right',
+    location:'Tags'
+  }
+
+  render(){
+    return (
+      
+      <div className="chart">
+        <Bar
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Language Tags composition '+this.props.location,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
+        />
+
+      </div>
+    )
+  }
+}
+
+export default BarChart;
